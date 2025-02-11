998466cdc87bc07aef4e6966d740663d9dc6431b
Kehinde Olaleye Simeon <60666942+DeEminentCoder@users.noreply.github.com>
2025-02-11 13:22:35 +0100

    commited grace

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -195,7 +195,7 @@ const Contact = () => {
           />
         </div>
       </div>
-      <div className="form-wrapper">
+      <div className="form-wrapper">ccc
         <label htmlFor="message" className="form-label">
           <i className="fa fa-edit"></i> Message
         </label>
