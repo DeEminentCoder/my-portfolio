b5665aba31e633f7f7f3575c51366f1ac5e162af
Kehinde Olaleye Simeon <60666942+DeEminentCoder@users.noreply.github.com>
2025-02-13 11:06:31 +0100

    fix: remove stray character in form-wrapper div in Contact component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -195,7 +195,7 @@ const Contact = () => {
           />
         </div>
       </div>
-      <div className="form-wrapper">io
+      <div className="form-wrapper">i
         <label htmlFor="message" className="form-label">
           <i className="fa fa-edit"></i> Message
         </label>
