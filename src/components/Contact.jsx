85b8dcea1362a2f3847c637a6980bec4c3b84720
Kehinde Olaleye Simeon <60666942+DeEminentCoder@users.noreply.github.com>
2025-02-15 13:24:17 +0100

    fix: remove stray characters from phone number in Contact component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -89,7 +89,7 @@ const Contact = () => {
                   </a>
 
                   <a href="tel:08159725758" className="contact-info">
-                    (234) 815 972 5758 vvvvff
+                    (234) 815 972 5758 vvvvf
                   </a>
                 </div>
               </li>
