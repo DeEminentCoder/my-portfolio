02ab53423745059dc869fcd84a6862add6c9bc81
Kehinde Olaleye Simeon <60666942+DeEminentCoder@users.noreply.github.com>
2025-03-11 16:47:06 +0100

    fix: remove stray character from Contact component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,7 @@
 // eslint-disable-next-line no-unused-vars
 import React, { useState } from 'react';
 import "../Contact.css";
-vvb
+vv
 const Contact = () => {
     const [formData, setFormData] = useState({
       name: '',
