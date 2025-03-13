3b3a1b760f5737b490b2daf8bb3506ce8e35a899
Kehinde Olaleye Simeon <60666942+DeEminentCoder@users.noreply.github.com>
2025-03-13 10:33:24 +0100

    jjones

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,7 @@
 // eslint-disable-next-line no-unused-vars
 import React, { useState } from 'react';
 import "../Contact.css";
-3e33
+3e3
 const Contact = () => {
     const [formData, setFormData] = useState({
       name: '',
