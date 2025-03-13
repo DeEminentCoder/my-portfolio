b1dfe59926a2e92b7384938a0422cbc768673146
Kehinde Olaleye Simeon <60666942+DeEminentCoder@users.noreply.github.com>
2025-03-13 14:00:20 +0100

    Fanta

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,7 @@
 // eslint-disable-next-line no-unused-vars
 import React, { useState } from 'react';
 import "../Contact.css";
-3e3
+3e
 const Contact = () => {
     const [formData, setFormData] = useState({
       name: '',
