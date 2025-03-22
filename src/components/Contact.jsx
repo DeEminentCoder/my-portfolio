bb0f955c4d9c62a050060e0ea0f09357c8461aa1
Kehinde Olaleye Simeon <60666942+DeEminentCoder@users.noreply.github.com>
2025-03-22 15:30:30 +0100

    fix: correct typo in Contact component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,7 @@
 // eslint-disable-next-line no-unused-vars
 import React, { useState } from 'react';
 import "../Contact.css";
-SEE
+SE
 const Contact = () => {
     const [formData, setFormData] = useState({
       name: '',
