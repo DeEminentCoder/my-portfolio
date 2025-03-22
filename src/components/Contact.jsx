c7dca1bac6465a679b464f4e97d610ab2e3775d6
Kehinde Olaleye Simeon <60666942+DeEminentCoder@users.noreply.github.com>
2025-03-22 15:39:29 +0100

    fix: set document title in Contact component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,7 @@
 // eslint-disable-next-line no-unused-vars
 import React, { useState } from 'react';
 import "../Contact.css";
-dv
+document.title = "Contact | DeEminent Coder";
 const Contact = () => {
     const [formData, setFormData] = useState({
       name: '',
