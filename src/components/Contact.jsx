5e634ae577facaae2b19139b049da429aa2b332c
Kehinde Olaleye Simeon <60666942+DeEminentCoder@users.noreply.github.com>
2025-03-26 14:56:45 +0100

    fix: remove extraneous characters in Contact component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -16,7 +16,7 @@ const Contact = () => {
         ...prevData,
         [name]: value,
       }));
-    };x
+    };xcss
   
     const handleSubmit = async (e) => {
       e.preventDefault(); // Prevent the default form submission behavior
