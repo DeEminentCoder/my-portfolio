24e7784244b2482d22dd945319ef0652eff124c8
Kehinde Olaleye Simeon <60666942+DeEminentCoder@users.noreply.github.com>
2025-04-02 17:58:01 +0100

    fix: remove invalid characters in Contact component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,7 +9,7 @@ const Contact = () => {
       email: '',
       phone: '',
       message: '',
-    });<NNMMK></NNMMK>
+    });<NNMM></NNMM
     const handleChange = (e) => {
       const { name, value } = e.target;
       setFormData((prevData) => ({
