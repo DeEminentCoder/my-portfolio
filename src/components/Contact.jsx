772cf132eefba407c9e517a96a50724c03be20ff
Kehinde Olaleye Simeon <60666942+DeEminentCoder@users.noreply.github.com>
2025-04-04 10:28:26 +0100

    fix: remove extraneous characters in Contact component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,7 +9,7 @@ const Contact = () => {
       email: '',
       phone: '',
       message: '',
-    });vb,
+    });v
     const handleChange = (e) => {
       const { name, value } = e.target;
       setFormData((prevData) => ({
