617b223bc8ea8acb63464b1697714c9249ba0b87
Kehinde Olaleye Simeon <60666942+DeEminentCoder@users.noreply.github.com>
2025-04-08 11:18:34 +0100

    fix: remove extraneous characters in handleChange function

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -15,7 +15,7 @@ const Contact = () => {
       setFormData((prevData) => ({
         ...prevData,
         [name]: value,
-      }));rrrrrr,
+      }));rrrrr,
     };
   
     const handleSubmit = async (e) => {
