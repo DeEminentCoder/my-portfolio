e70210ca929e544de35281d771fa357ab7613d77
Kehinde Olaleye Simeon <60666942+DeEminentCoder@users.noreply.github.com>
2025-04-08 09:56:47 +0100

    aorye

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -15,7 +15,7 @@ const Contact = () => {
       setFormData((prevData) => ({
         ...prevData,
         [name]: value,
-      }));
+      }));rrrrrrrrrrr
     };
   
     const handleSubmit = async (e) => {
